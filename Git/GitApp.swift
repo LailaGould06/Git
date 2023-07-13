a4be0a03decbb534d97f1fd9837bea7e4121b701
Laila Gould <lailagould10@gmail.com>
2023-07-13 13:33:04 -0500

    Initial Commit

---
+++ Git/GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitApp.swift
+//  Git
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
